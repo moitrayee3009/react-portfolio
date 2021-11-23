8ae36f31f098e1304e82c35669097869a61cd2cd
Moitrayee Choudhury <moitrayee3009@gmail.com>
2021-11-23 18:16:33 +0100

    updated

--- src/components/Education.js
+++ src/components/Education.js
@@ -51,37 +51,6 @@ class Education extends Component {
           </Grid>
         ))}
       </Fragment>
-
-      /* <Grid className="section" style={{ marginTop: '30px' }}>
-
-        <Cell col={4}>
-          <p>{this.state.startYearKYH} - {this.state.endYearKYH}</p>
-        </Cell>
-        <Cell col={8}>
-          <h4 style={{ marginTop: '0px' }}>{this.state.schoolNameKYH}</h4>
-          <p>{this.state.schoolDescriptionKYH}</p>
-        </Cell> */
-
-      /*Bangladesh  */
-
-      /* <Cell col={4}>
-          <p>{this.state.startYearMS} - {this.state.endYearMS}</p>
-        </Cell>
-        <Cell col={8}>
-          <h4 style={{ marginTop: '0px' }}>{this.state.schoolName}</h4>
-          <p>{this.state.schoolDescriptionMS}</p>
-        </Cell>
-
-        <Cell col={4}>
-          <p>{this.state.startYear} - {this.state.endYear}</p>
-        </Cell>
-        <Cell col={8}>
-          <h4 style={{ marginTop: '0px' }}>{this.state.schoolName}</h4>
-          <p>{this.state.schoolDescription}</p>
-        </Cell>
-
-
-      </Grid> */
     )
   }
 }
